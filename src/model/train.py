c2c387b75d2820b08ee353e16b93feed45551546
José Tapia <tapiatellez@gmail.com>
2022-11-24 05:22:38 +0000

    Correct linting error from the train.py file

--- src/model/train.py
+++ src/model/train.py
@@ -8,7 +8,7 @@ import mlflow
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split 
+from sklearn.model_selection import train_test_split
 
 
 # define functions
@@ -37,9 +37,13 @@ def get_csvs_df(path):
 
 # TO DO: add function to split data
 def split_data(df):
-    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness', 'SerumInsulin', 'BMI', 'DiabetesPedigree', 'Age']].values, df['Diabetic'].values
+    X_train, X_test, y_train, y_test = train_test_split(X,
+                                                        y,
+                                                        test_size=0.30,
+                                                        random_state=0)
     return X_train, X_test, y_train, y_test
+
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
@@ -61,6 +65,7 @@ def parse_args():
 
     # return args
     return args
+
 
 # run script
 if __name__ == "__main__":
